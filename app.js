a0a4e5fa5e2f2b7de1aef7374bcccbfb8a624d7a
Akshaykumar Bawane <messier877@gmail.com>
2026-03-03 00:57:30 +0530

    Fix Binding Issue

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const server = http.createServer((req, res) => {
   res.send("AUTO CD SUCCESSFULLY WORKING... 🔥🔥🔥");
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(3000, '0.0.0.0', () => {
+  console.log("Server running on 0.0.0.0");
 });
